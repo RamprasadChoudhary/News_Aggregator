5ea92e3e7163bbc4aaef1f4b209fa015dbe6f453
RAM PRASAD CHOUDHARY <116368554+RamprasadChoudhary@users.noreply.github.com>
2024-12-29 15:10:01 +0530

    Update AllNews.jsx

--- client/src/components/AllNews.jsx
+++ client/src/components/AllNews.jsx
@@ -22,7 +22,7 @@ function AllNews() {
   useEffect(() => {
     setIsLoading(true);
     setError(null);
-    fetch(`https://news-aggregator-vwl5.onrender.com/all-news?page=${page}&pageSize=${pageSize}`)
+    fetch(`https://news-aggregator-backend-cglz.onrender.com/all-news?page=${page}&pageSize=${pageSize}`)
       .then(response => {
         if (response.ok) {
           return response.json();
